2e977e45253dc3d7150623d620f3ed581a11bad5
Tim Pansino <timpansino@gmail.com>
2021-11-09 14:02:26 -0800

    Move logs location

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-newrelic-admin run-program gunicorn main:app -w 3 -t 120 >>/var/log/python.log 2>&1 &
-locust --headless --users 5 --spawn-rate 1 -f locustfile.py >>/var/log/locust.log 2>&1
+newrelic-admin run-program gunicorn main:app -w 3 -t 120 >>./logs/python.log 2>&1 &
+locust --headless --users 5 --spawn-rate 1 -f locustfile.py >>./logs/locust.log 2>&1
