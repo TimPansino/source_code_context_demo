5b95c3160fbfe6caf60c07ab58586e902f9156c1
Tim Pansino <timpansino@gmail.com>
2021-11-15 17:00:05 -0800

    Update to new demo version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-newrelic-admin run-program gunicorn main:app -w 3 -t 120 >>./logs/python.log 2>&1 &
+# Tee logs so application logs show up in stdout as well as logfile
+newrelic-admin run-program gunicorn main:app -b 0.0.0.0:8000 -w 3 -t 120 | tee -a ./logs/python.log 2>&1 &
+
+# Redirect logs without tee to remove spam from traffic driver
 locust --headless --users 5 --spawn-rate 1 -f locustfile.py >>./logs/locust.log 2>&1
